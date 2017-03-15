645693838dcf5779a3603bd247c8ded77b470d8e
dreikaa <dreiglushko@gmail.com>
2017-03-15 23:52:09 +0300

    commented controllers

--- src/main/java/com/ncedu/testing/controller/QuestionController.java
+++ src/main/java/com/ncedu/testing/controller/QuestionController.java
@@ -1,11 +1,12 @@
-package com.ncedu.testing.controller;
-
-import org.springframework.stereotype.Controller;
-
-/**
- * Created by dreikaa on 3/12/17.
- */
-@Controller
-public class QuestionController {
-
-}
+//package com.ncedu.testing.controller;
+//
+//import org.springframework.stereotype.Controller;
+//
+///**
+// * Created by dreikaa on 3/12/17.
+// */
+//@Controller
+//public class QuestionController {
+//
+//}
+//
--- src/main/java/com/ncedu/testing/controller/TestController.java
+++ src/main/java/com/ncedu/testing/controller/TestController.java
@@ -1,10 +1,10 @@
-package com.ncedu.testing.controller;
-
-import org.springframework.stereotype.Controller;
-
-/**
- * Created by dreikaa on 3/12/17.
- */
-@Controller
-public class TestController {
-}
+//package com.ncedu.testing.controller;
+//
+//import org.springframework.stereotype.Controller;
+//
+///**
+// * Created by dreikaa on 3/12/17.
+// */
+//@Controller
+//public class TestController {
+//}
